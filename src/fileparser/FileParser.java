ccc7eef321384a432d6a0a87259af0c5fa216daf
R-Mule <asmit119@me.com>
2017-12-18 15:44:17 -0500

    Test Commit2

--- src/fileparser/FileParser.java
+++ src/fileparser/FileParser.java
@@ -4,11 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *TEST2
